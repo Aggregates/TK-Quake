304bca3649c177831eb6043cf0f4ea182e2750ec
Simon Hartcher <simon@simonhartcher.com>
2015-08-03 19:05:07 +1000

    Remove System.Drawing from GameLoop.Cookbook

--- src/GameLoop.Cookbook/Program.cs
+++ src/GameLoop.Cookbook/Program.cs
@@ -5,7 +5,6 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,7 +40,7 @@ namespace GameLoop.Cookbook
         private void game_RenderFrame(object sender, FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-           
+
             /*
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
