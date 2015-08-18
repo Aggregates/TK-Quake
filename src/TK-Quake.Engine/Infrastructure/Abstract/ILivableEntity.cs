f54b6f0da421779b4ec3813985db6f16ba3c607b
Tyler Haigh <tylerhaigh1994@me.com>
2015-08-19 09:16:17 +1000

    Defines the Interfaces that define a livable player within the game world

---
+++ src/TK-Quake.Engine/Infrastructure/Abstract/ILivableEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKQuake.Engine.Infrastructure.Math;
+
+namespace TKQuake.Engine.Infrastructure.Abstract
+{
+    /// <summary>
+    /// A livable entity that resides in the game world such as the player or NPC
+    /// </summary>
+    public interface ILivableEntity
+    {
+        void Move(Vector position);
+
+        /// <summary>
+        /// Creates the object in the game world by making it "live"
+        /// </summary>
+        void Spawn();
+
+        /// <summary>
+        /// Kills the object and removes it from the world
+        /// </summary>
+        void Die();
+
+        void Hit(double damage);
+    }
+}
