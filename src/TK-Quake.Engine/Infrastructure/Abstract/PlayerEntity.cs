382de6b160466a16287033b1c14a5d652ba669ca
Tyler Haigh <tylerhaigh1994@me.com>
2015-08-19 09:16:17 +1000

    Defines the Interfaces that define a livable player within the game world

---
+++ src/TK-Quake.Engine/Infrastructure/Abstract/PlayerEntity.cs
@@ -0,0 +1,51 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKQuake.Engine.Infrastructure.Math;
+using TKQuake.Engine.Infrastructure.Texture;
+
+namespace TKQuake.Engine.Infrastructure.Abstract
+{
+    public abstract class PlayerEntity : ILivableEntity, IGameObject
+    {
+        public double Health { get; set;}
+        public Vector Position { get; set; }
+        public double MoveSpeed { get; set; }
+        public double RotationSpeed { get; set; }
+        public Matrix4 Matrix { get; private set; }
+        public bool Alive { get; private set; }
+        public Sprite2 Sprite { get; set; }
+
+        public virtual void Move(Vector position)
+        {
+            this.Position = position;
+        }
+
+        public virtual void Spawn()
+        {
+            Alive = true;
+        }
+
+        public virtual void Die()
+        {
+            Alive = false;
+        }
+
+        public virtual void Hit(double damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+                Die();
+        }
+
+        public virtual void Update(double elapsedTime) { }
+
+        public virtual void Render()
+        {
+            Sprite.Render();
+        }
+    }
+}
