37a5c15c4e29fd9572bf1077ba7173b8f2d150e1
Tyler Haigh <tylerhaigh1994@me.com>
2015-08-20 20:46:57 +1000

    Fix invalid behaviour of MatrixLookAt extension method
    The standard use of this method on OpenGL is to pass in the addition of
    the Position and LookAt vectors for the viewDirection argument. This
    method was adding the two a second time causing erroneous behaviour when
    working with the camera movement feature

--- src/TK-Quake.Engine/Extensions/GLX.cs
+++ src/TK-Quake.Engine/Extensions/GLX.cs
@@ -20,13 +20,22 @@ namespace TKQuake.Engine.Extensions
             GL.Color3(c.R, c.G, c.B);
         }
 
+        /// <summary>
+        /// Extension of Matrix4.LookAt to work with engine defined vectors. When calling this
+        /// method, make sure to add the Position to the LookAt normalised directional vector
+        /// for the viewDirection argument.
+        /// </summary>
+        /// <param name="position">The current position of the entity</param>
+        /// <param name="viewDirection">The position added to the LookAt direction</param>
+        /// <param name="up">The upward axis direction</param>
+        /// <returns></returns>
         public static Matrix4 MarixLookAt(Vector position, Vector viewDirection, Vector up)
         {
             Vector3 pos = new Vector3((float)position.X, (float)position.Y, (float)position.Z);
             Vector3 view = new Vector3((float)viewDirection.X, (float)viewDirection.Y, (float)viewDirection.Z);
             Vector3 upVec = new Vector3((float)up.X, (float)up.Y, (float)up.Z);
 
-            return Matrix4.LookAt(pos, pos + view, upVec);
+            return Matrix4.LookAt(pos, view, upVec);
         }
     }
 }
