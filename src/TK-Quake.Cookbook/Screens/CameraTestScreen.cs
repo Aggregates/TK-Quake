6959eb9550372b31ac75abb87a6c024c8c31d4d4
Simon Hartcher <simon@simonhartcher.com>
2015-08-25 00:00:56 +1000

    Further cleanup of Camera testing

--- src/TK-Quake.Cookbook/Screens/CameraTestScreen.cs
+++ src/TK-Quake.Cookbook/Screens/CameraTestScreen.cs
@@ -22,69 +22,14 @@ namespace TKQuake.Cookbook.Screens
         public CameraTestScreen()
         {
             Children.Add(_camera);
-        }
-
-        private void HandleInput(Key k)
-        {
-            switch(k)
-            {
-                case Key.W:
-                    {
-                        // Forward
-                        double x = (float)Math.Cos(_camera.Rotation.Y + Math.PI / 2);
-                        double z = (float)Math.Sin(_camera.Rotation.Y + Math.PI / 2);
-                        _camera.Move(-x, 0, -z);
-                        break;
-                    }
-
-                case Key.S:
-                    {
-                        // Back
-                        double x = (float)Math.Cos(_camera.Rotation.Y + Math.PI / 2);
-                        double z = (float)Math.Sin(_camera.Rotation.Y + Math.PI / 2);
-                        _camera.Move(x, 0, z);
-                        break;
-                    }
-
-                case Key.A:
-                    {
-                        // Strafe left
-                        double x = (float)Math.Cos(_camera.Rotation.Y);
-                        double z = (float)Math.Sin(_camera.Rotation.Y);
-                        _camera.Move(-x, 0, -z);
-                        break;
-                    }
-                case Key.D:
-                    {
-                        // Strafe right
-                        double x = (float)Math.Cos(_camera.Rotation.Y);
-                        double z = (float)Math.Sin(_camera.Rotation.Y);
-                        _camera.Move(x, 0, z);
-                        break;
-                    }
-                case Key.Left: { _camera.Rotate(0, -1, 0); break; }
-                case Key.Right: { _camera.Rotate(0, 1, 0); break; }
-                case Key.Up: { _camera.Rotate(1, 0, 0); break; }
-                case Key.Down: { _camera.Rotate(-1, 0, 0); break; }
-            }
+            Components.Add(new MovementInputComponent(_camera));
         }
 
         public new void Update(double elapsedTime)
         {
             base.Update(elapsedTime);
 
-            var state = Keyboard.GetState();
-            if (state[Key.W]) HandleInput(Key.W);
-            if (state[Key.A]) HandleInput(Key.A);
-            if (state[Key.S]) HandleInput(Key.S);
-            if (state[Key.D]) HandleInput(Key.D);
-            if (state[Key.Left]) HandleInput(Key.Left);
-            if (state[Key.Right]) HandleInput(Key.Right);
-            if (state[Key.Up]) HandleInput(Key.Up);
-            if (state[Key.Down]) HandleInput(Key.Down);
-
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
             GL.Enable(EnableCap.DepthTest);
 
             // Display some planes
@@ -110,6 +55,74 @@ namespace TKQuake.Cookbook.Screens
                 }
             }
 
+        }
+    }
+
+    class MovementInputComponent : IComponent
+    {
+        private readonly PlayerEntity _entity;
+        public MovementInputComponent(PlayerEntity entity)
+        {
+            _entity = entity;
+        }
+
+        public void Startup() { }
+        public void Shutdown() { }
+        public void Update(double elapsedTime) {
+            var state = Keyboard.GetState();
+            if (state[Key.W]) HandleInput(Key.W);
+            if (state[Key.A]) HandleInput(Key.A);
+            if (state[Key.S]) HandleInput(Key.S);
+            if (state[Key.D]) HandleInput(Key.D);
+            if (state[Key.Left]) HandleInput(Key.Left);
+            if (state[Key.Right]) HandleInput(Key.Right);
+            if (state[Key.Up]) HandleInput(Key.Up);
+            if (state[Key.Down]) HandleInput(Key.Down);
+        }
+
+        public void HandleInput(Key k)
+        {
+            switch(k)
+            {
+                case Key.W:
+                    {
+                        // Forward
+                        double x = (float)Math.Cos(_entity.Rotation.Y + Math.PI / 2);
+                        double z = (float)Math.Sin(_entity.Rotation.Y + Math.PI / 2);
+                        _entity.Move(-x, 0, -z);
+                        break;
+                    }
+
+                case Key.S:
+                    {
+                        // Back
+                        double x = (float)Math.Cos(_entity.Rotation.Y + Math.PI / 2);
+                        double z = (float)Math.Sin(_entity.Rotation.Y + Math.PI / 2);
+                        _entity.Move(x, 0, z);
+                        break;
+                    }
+
+                case Key.A:
+                    {
+                        // Strafe left
+                        double x = (float)Math.Cos(_entity.Rotation.Y);
+                        double z = (float)Math.Sin(_entity.Rotation.Y);
+                        _entity.Move(-x, 0, -z);
+                        break;
+                    }
+                case Key.D:
+                    {
+                        // Strafe right
+                        double x = (float)Math.Cos(_entity.Rotation.Y);
+                        double z = (float)Math.Sin(_entity.Rotation.Y);
+                        _entity.Move(x, 0, z);
+                        break;
+                    }
+                case Key.Left: { _entity.Rotate(0, -1, 0); break; }
+                case Key.Right: { _entity.Rotate(0, 1, 0); break; }
+                case Key.Up: { _entity.Rotate(1, 0, 0); break; }
+                case Key.Down: { _entity.Rotate(-1, 0, 0); break; }
+            }
         }
     }
 }
