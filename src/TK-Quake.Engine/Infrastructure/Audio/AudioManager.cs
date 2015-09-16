321080d4cba437144ebcdc77962e3fcc2e5ef271
Jake Goodsir <jgoodsir1210@gmail.com>
2015-09-16 16:17:15 +1000

    init feature AudioManager

---
+++ src/TK-Quake.Engine/Infrastructure/Audio/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
+
+namespace TKQuake.Engine.Infrastructure.Audio
+{
+    class AudioManager
+    {
+
+        public AudioManager() { }
+
+        public static void AudioTest()
+        {
+
+        }
+    }
+
+}
