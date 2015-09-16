780d0c81570bf41d25e693851d45865cc57178c6
jakesg <jgoodsir1210@gmail.com>
2015-09-16 18:50:06 +1000

    Plays a .wav music file named "music" using OpenAL tutorial methods.

--- src/TK-Quake.Engine/Infrastructure/Audio/AudioManager.cs
+++ src/TK-Quake.Engine/Infrastructure/Audio/AudioManager.cs
@@ -3,19 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using OpenTK.Audio.OpenAL;
+using OpenTK.Audio;
+using System.Threading;
 
+
+//This is not yet a manager, just me using a tutorial and making sure I could get it working.
+//If NotSupportedException's occur, the music file probably isn't following the strict format
 namespace TKQuake.Engine.Infrastructure.Audio
 {
-    class AudioManager
+    public class AudioManager
     {
+        static readonly string filename = Path.Combine("Audio", "music.wav");
 
-        public AudioManager() { }
-
-        public static void AudioTest()
+        // Loads a wave/riff audio file.
+        public static byte[] LoadWave(Stream stream, out int channels, out int bits, out int rate)
         {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
 
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                // RIFF header
+                string signature = new string(reader.ReadChars(4));
+                if (signature != "RIFF")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                int riff_chunk_size = reader.ReadInt32();
+
+                string format = new string(reader.ReadChars(4));
+                if (format != "WAVE")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                // WAVE header
+                string format_signature = new string(reader.ReadChars(4));
+                if (format_signature != "fmt ")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int format_chunk_size = reader.ReadInt32();
+                int audio_format = reader.ReadInt16();
+                int num_channels = reader.ReadInt16();
+                int sample_rate = reader.ReadInt32();
+                int byte_rate = reader.ReadInt32();
+                int block_align = reader.ReadInt16();
+                int bits_per_sample = reader.ReadInt16();
+
+                string data_signature = new string(reader.ReadChars(4));
+                if (data_signature != "data") throw new NotSupportedException("Specified wave file is not supported.");
+
+                int data_chunk_size = reader.ReadInt32();
+
+                channels = num_channels;
+                bits = bits_per_sample;
+                rate = sample_rate;
+
+                return reader.ReadBytes((int)reader.BaseStream.Length);
+            }
+        }
+
+        //Returns the correct output format
+        public static ALFormat GetSoundFormat(int channels, int bits)
+        {
+            switch (channels)
+            {
+                case 1: return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
+                case 2: return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
+                default: throw new NotSupportedException("The specified sound format is not supported.");
+            }
+        }
+
+        //Loads the file, then plays it once.
+        public static void Play()
+        {
+            using (AudioContext context = new AudioContext())
+            {
+                int buffer = AL.GenBuffer();
+                int source = AL.GenSource();
+                int state;
+
+                int channels, bits_per_sample, sample_rate;
+                byte[] sound_data = LoadWave(File.Open(filename, FileMode.Open), out channels, out bits_per_sample, out sample_rate);
+                AL.BufferData(buffer, GetSoundFormat(channels, bits_per_sample), sound_data, sound_data.Length, sample_rate);
+
+                AL.Source(source, ALSourcei.Buffer, buffer);
+                AL.SourcePlay(source);
+
+                
+                Console.WriteLine("Playing");
+
+                // Query the source to find out when it stops playing.
+                do
+                {
+                    Thread.Sleep(250);
+                    Console.Write(".");
+                    AL.GetSource(source, ALGetSourcei.SourceState, out state);
+                }
+                while ((ALSourceState)state == ALSourceState.Playing);
+
+                Console.WriteLine("");
+
+                AL.SourceStop(source);
+                AL.DeleteSource(source);
+                AL.DeleteBuffer(buffer);
+            }
         }
     }
 
 }
+
