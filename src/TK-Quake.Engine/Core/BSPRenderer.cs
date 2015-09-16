b47d6ddc541ea7bc50ca6eec2097d4d29c971362
Alex Biddulph <c3124185@uon.edu.au>
2015-09-17 06:12:27 +1000

    Neatened up swizzle code.

--- src/TK-Quake.Engine/Core/BSPRenderer.cs
+++ src/TK-Quake.Engine/Core/BSPRenderer.cs
@@ -15,7 +15,7 @@ namespace TKQuake.Engine.Core
 {
     public class BSPRenderer
     {
-        private const int TESSELLATION_LEVEL = 7;
+        private const int TESSELLATION_LEVEL = 3;
 
         private string BSPFile;
         private Loader.BSPLoader BSP = null;
@@ -43,7 +43,7 @@ namespace TKQuake.Engine.Core
                 BSP = null;
             }
 
-            BSP = new Loader.BSPLoader (BSPFile, false);
+            BSP = new Loader.BSPLoader (BSPFile, true);
             BSP.LoadFile ();
             BSP.DumpBSP ();
         }
