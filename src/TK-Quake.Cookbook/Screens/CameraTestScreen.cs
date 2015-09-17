a148a8a5b11f20a2c208aa05628680a254864205
jakesg <jgoodsir1210@gmail.com>
2015-09-17 23:05:45 +1000

    Hardcoded to be able to play the Quake theme.

--- src/TK-Quake.Cookbook/Screens/CameraTestScreen.cs
+++ src/TK-Quake.Cookbook/Screens/CameraTestScreen.cs
@@ -58,7 +58,7 @@ namespace TKQuake.Cookbook.Screens
             //Might be to do with where I have declared it.
             //Thread th = new Thread(new ThreadStart(AudioManager.Play));
             //th.Start();
-            var filename = Path.Combine("Audio", "music.wav");
+            var filename = Path.Combine("Audio", "QuakeTheme.wav");
             new Thread(delegate ()
             {
                 using (AudioContext context = new AudioContext())
@@ -66,6 +66,7 @@ namespace TKQuake.Cookbook.Screens
                     _audioManager.Add("bgm", filename);
                     _audioManager.Play("bgm");
                 }
+                //_audioManager.printHeader(filename);
             }).Start();
 
         }
