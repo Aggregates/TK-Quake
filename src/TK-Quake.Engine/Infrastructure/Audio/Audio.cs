7c71bbae4262e18fa57c7039c80475c8274cfff3
jakesg <jgoodsir1210@gmail.com>
2015-09-17 10:51:54 +1000

    Audio class for storing audio data and information about the data

---
+++ src/TK-Quake.Engine/Infrastructure/Audio/Audio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKQuake.Engine.Infrastructure.Audio
+{
+    public struct Audio
+    {
+        public int Id { get; set; }
+        public byte[] AudioData { get; set; }
+        public int Channels { get; set; }
+        public int Bits { get; set; }
+        public int Rate { get; set; }
+
+        public Audio (int id, byte[] data, int channels, int bits, int rate)
+        {
+            this.Id = id;
+            this.AudioData = data;
+            this.Channels = channels;
+            this.Bits = bits;
+            this.Rate = rate;
+        }
+        //public int Channels { get; set; }
+        //public int Bits { get; set; }
+        //public int Rate { get; set; }
+        
+        //public Audio (int channels, int bits, int rate)
+        //{
+        //    this.Channels = channels;
+        //    this.Bits = bits;
+        //    this.Rate = rate;
+        //}
+
+    }
+}
