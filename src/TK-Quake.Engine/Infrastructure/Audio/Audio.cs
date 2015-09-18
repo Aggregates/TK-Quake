0648360ec0398957fc3d92350f065fe45240640c
jakesg <jgoodsir1210@gmail.com>
2015-09-18 13:18:40 +1000

    Plays OGG files (probably) using DragonOGG library http://sourceforge.net/projects/dragonogg/

--- src/TK-Quake.Engine/Infrastructure/Audio/Audio.cs
+++ src/TK-Quake.Engine/Infrastructure/Audio/Audio.cs
@@ -13,14 +13,26 @@ namespace TKQuake.Engine.Infrastructure.Audio
         public int Channels { get; set; }
         public int Bits { get; set; }
         public int Rate { get; set; }
+        public string FileName { get; set; }
 
-        public Audio (int id, byte[] data, int channels, int bits, int rate)
+        public Audio (int id, byte[] data, int channels, int bits, int rate, string filename)
         {
             this.Id = id;
             this.AudioData = data;
             this.Channels = channels;
             this.Bits = bits;
             this.Rate = rate;
+            this.FileName = filename;
+        }
+
+        public Audio(string filename) //for OGG
+        {
+            Id = -1;
+            AudioData = null;
+            Channels = -1;
+            Bits = -1;
+            Rate = -1;
+            this.FileName = filename;
         }
         //public int Channels { get; set; }
         //public int Bits { get; set; }
